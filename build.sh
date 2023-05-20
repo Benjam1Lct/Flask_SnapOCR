0f6c1cbbd0b968cb5b61a596ac0fe3a2e63a80df
PearlClone <benjaminlecomte37@gmail.com>
2023-05-20 14:16:06 +0200

    update

--- build.sh
+++ build.sh
@@ -2,5 +2,18 @@
 # exit on error
 set -o errexit
 
-pip install --upgrade pip
+python -m venv auth
+source auth/Scripts/activate
+
 pip install -r project/requirements.txt
+
+file_path="./auth/Lib/site-packages/flask_uploads.py"
+
+# Effectuer le remplacement des lignes spécifiques
+sed -i 's/from werkzeug import secure_filename, FileStorage/from werkzeug.utils import secure_filename\nfrom werkzeug.datastructures import FileStorage/' $file_path
+
+export FLASK_APP=project
+export FLASK_DEBUG=1
+export FLASK_RUN_HOST=0.0.0.0
+
+flask run
--- project/models.py
+++ project/models.py
@@ -4,7 +4,7 @@ from . import db
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(100), unique=True)
-    password = db.Column(db.String(100), min_length=8)
+    password = db.Column(db.String(100))
     name = db.Column(db.String(1000))
     predictions = db.relationship('Predict', backref='user', lazy=True)
 
