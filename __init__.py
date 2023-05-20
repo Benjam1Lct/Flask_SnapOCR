7b6652ab06cdd487b21a156a44511cdb41859ce1
PearlClone <benjaminlecomte37@gmail.com>
2023-05-20 18:03:57 +0200

    update

--- __init__.py
+++ __init__.py
@@ -2,7 +2,6 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_uploads import UploadSet, configure_uploads, IMAGES
 from flask_login import LoginManager
-from flask_migrate import Migrate
 from . import config ,data_generation, datasets, detection, evaluation, pipeline, recognition, tools
 
 
