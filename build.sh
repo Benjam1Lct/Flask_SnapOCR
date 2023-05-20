a046b3b3fa82d9195ee35c5037ea8b1903c07f5b
PearlClone <benjaminlecomte37@gmail.com>
2023-05-20 14:21:20 +0200

    update

--- build.sh
+++ build.sh
@@ -11,9 +11,3 @@ file_path="./auth/Lib/site-packages/flask_uploads.py"
 
 # Effectuer le remplacement des lignes spécifiques
 sed -i 's/from werkzeug import secure_filename, FileStorage/from werkzeug.utils import secure_filename\nfrom werkzeug.datastructures import FileStorage/' $file_path
-
-export FLASK_APP=project
-export FLASK_DEBUG=1
-export FLASK_RUN_HOST=0.0.0.0
-
-flask run
---
+++ start.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+source auth/Scripts/activate
+
+export FLASK_APP=project
+export FLASK_DEBUG=1
+export FLASK_RUN_HOST=0.0.0.0
+
+flask run
