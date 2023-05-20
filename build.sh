ce2382bc8f5af5278a81554ec4f5c0bc0a33dddf
PearlClone <benjaminlecomte37@gmail.com>
2023-05-20 14:40:21 +0200

    up

--- build.sh
+++ build.sh
@@ -7,13 +7,14 @@ python -m venv auth
 
 pip install -r project/requirements.txt
 
-# Recherche du fichier flask_uploads.py dans le répertoire d'installation de Flask
-file_path=$(find "$PWD/auth" -type f -name "flask_uploads.py")
+# Rechercher le fichier flask_uploads.py et récupérer son chemin
+file_path=$(find "$PWD/auth/Lib/site-packages" -name "flask_uploads.py")
 
+# Vérifier si le fichier a été trouvé
 if [[ -n $file_path ]]; then
-    # Effectuer le remplacement des lignes spécifiques
-    sed -i 's/from werkzeug import secure_filename, FileStorage/from werkzeug.utils import secure_filename\nfrom werkzeug.datastructures import FileStorage/' "$file_path"
+  # Effectuer le remplacement des lignes spécifiques
+  sed -i 's/from werkzeug import secure_filename, FileStorage/from werkzeug.utils import secure_filename\nfrom werkzeug.datastructures import FileStorage/' "$file_path"
 else
-    echo "Erreur : fichier flask_uploads.py introuvable."
-    exit 1
+  echo "Le fichier flask_uploads.py n'a pas été trouvé."
+  exit 1
 fi
