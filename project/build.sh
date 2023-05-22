df5a9892a725a4b6848702b575a3b4738a5e7d9c
PearlClone <80157210+PearlClone@users.noreply.github.com>
2023-05-22 15:38:22 +0200

    Update build.sh

--- project/build.sh
+++ project/build.sh
@@ -8,9 +8,3 @@ python -m venv auth
 
 # Installer les dépendances dans l'environnement virtuel
 pip install -r project/requirements.txt
-
-# Chemin vers le fichier flask_uploads.py
-file_path="./Flask_SnapOCR-2auth/Lib/site-packages/flask_uploads.py"
-
-# Effectuer le remplacement des lignes spécifiques
-sed -i 's/from werkzeug import secure_filename, FileStorage/from werkzeug.utils import secure_filename\nfrom werkzeug.datastructures import FileStorage/' $file_path
