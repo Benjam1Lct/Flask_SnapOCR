45816ad3e1cee9caca0b91a1da2db0902ae7bd19
PearlClone <80157210+PearlClone@users.noreply.github.com>
2023-05-22 15:22:28 +0200

    Update build.sh

--- project/build.sh
+++ project/build.sh
@@ -1,2 +1,16 @@
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+# Créer et activer l'environnement virtuel
+python -m venv auth
+. "$PWD/auth/Scripts/activate"
+
+# Installer les dépendances dans l'environnement virtuel
 pip install -r project/requirements.txt
 
+# Chemin vers le fichier flask_uploads.py
+file_path="./Flask_SnapOCR-2auth/Lib/site-packages/flask_uploads.py"
+
+# Effectuer le remplacement des lignes spécifiques
+sed -i 's/from werkzeug import secure_filename, FileStorage/from werkzeug.utils import secure_filename\nfrom werkzeug.datastructures import FileStorage/' $file_path
