53a423445249ebe95f07e1c5b5b03374c21d2963
PearlClone <80157210+PearlClone@users.noreply.github.com>
2023-05-22 15:44:10 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,9 +2,7 @@
 # exit on error
 set -o errexit
 
-# Créer et activer l'environnement virtuel
 python -m venv auth
-. "$PWD/auth/Scripts/activate"
+source "$PWD/auth/Scripts/activate"
 
-# Installer les dépendances dans l'environnement virtuel
 pip install -r project/requirements.txt
