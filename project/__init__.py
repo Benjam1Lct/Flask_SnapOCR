606338dbc9590354212154f2f7338180d240d227
PearlClone <80157210+PearlClone@users.noreply.github.com>
2023-05-23 19:36:40 +0200

    Update __init__.py

--- project/__init__.py
+++ project/__init__.py
@@ -10,7 +10,8 @@ def create_app():
     app = Flask(__name__, template_folder='templates', static_folder='static')
 
     app.config['SECRET_KEY'] = 'thisisasecretkeyformyappflask'
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
+    #postgres://snapocr_postgres_user:Q6Dc6Hi9A1VtxUJajrxsOwHybVMQwYtA@dpg-chlqjre7avj2178s81cg-a.frankfurt-postgres.render.com/snapocr_postgres
 
     app.config['UPLOADED_PHOTOS_DEST'] = 'uploads'
     photos = UploadSet('photos', IMAGES)
