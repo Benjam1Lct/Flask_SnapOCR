787c044e7dbc86971c0ed43654e493580d63dc02
PearlClone <benjaminlecomte37@gmail.com>
2023-05-24 18:40:43 +0200

    update

--- build.sh
+++ build.sh
@@ -3,3 +3,8 @@
 set -o errexit
 
 pip install -r requirements.txt
+
+file_path="/opt/render/project/src/.venv/lib/python3.7/site-packages/flask_uploads.py"
+
+# Effectuer le remplacement des lignes spécifiques
+sed -i 's/from werkzeug import secure_filename, FileStorage/from werkzeug.utils import secure_filename\nfrom werkzeug.datastructures import FileStorage/' $file_path
