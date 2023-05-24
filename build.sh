6c624b2bdc622a7dea4d97c572e90c8a616565d2
PearlClone <benjaminlecomte37@gmail.com>
2023-05-24 18:36:52 +0200

    update

--- build.sh
+++ build.sh
@@ -2,8 +2,4 @@
 # exit on error
 set -o errexit
 
-python -m pip install virtualenv
-python -m virtualenv auth
-. auth/bin/activate
-
-pip install -r project/requirements.txt
+pip install -r requirements.txt
--- start.sh
+++ start.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/env bash
 # exit on error
-set -o errexit
 
-. auth/bin/activate
-
-gunicorn app:project
+gunicorn run:app
